fd6db5db9743f8a7a9d5d9f79e60d08ff06a3295
AntyufeevA <awsd.artem.2k00@gmail.com>
2021-09-22 21:33:06 +0500

    task02 ok

--- task02/src/com/example/task02/Task02.java
+++ task02/src/com/example/task02/Task02.java
@@ -4,18 +4,23 @@ public class Task02 {
 
     public static String solution(String input) {
 
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return "";
+        //
+        long number = Long.parseLong(input);
+        if (number >= -128 && number < 128)
+            return "byte";
+        if (number >= -32768 && number < 32768)
+            return "short";
+        if (number >= -2147483648 && number < 2147483648L)
+            return "int";
+        return "long";
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
+
+
         String result = solution("12345");
         System.out.println(result);
-         */
+
     }
 
 }
